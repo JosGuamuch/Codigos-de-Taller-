d3b5b9c3413908c1e6842bcd0d42a39c4ee8bd35
JosGuamuch <142423899+JosGuamuch@users.noreply.github.com>
2024-05-10 11:11:40 -0600

    Add files via upload

---
+++ Parte_2_de_Practica_5.ino
@@ -0,0 +1,241 @@
+
+
+
+
+float sol = 391.995;
+float mib = 311.127;
+float sib = 466.164;
+float RE = 293.665;
+float MIB = 311.127;
+float solb = 369.994;
+float SOL = 391.9954;
+float FAS = 369.994;
+float FA = 349.228;
+float MI = 329.995;
+float solS = 415.305;
+float DOS = 277.183;
+float DO = 261.626;
+float RES = 311.127 ;
+float la = 440.000; 
+float si = 493.883;
+
+void setup()
+{
+  tone(3,sol,300);
+  delay(600);
+  noTone(3);
+  tone(3,sol,300);
+  delay(600);
+  noTone(3);
+  tone(3,sol,300);
+  delay(600);
+  
+  tone(3,mib ,300);
+  delay(350);
+  noTone(3);
+  tone(3,sib,300);
+  delay(350);
+  noTone(3);
+  tone(3,sol,300);
+  delay(700);
+  noTone(3);
+  
+  tone(3,mib ,300);
+  delay(350);
+  noTone(3);
+  tone(3,sib,300);
+  delay(350);
+  noTone(3);
+  tone(3,sol,300);
+  delay(800);
+  noTone(3);
+  
+  tone(3,RE,300);
+  delay(600);
+  tone(3,RE,300);
+  delay(600);
+  tone(3,RE,300);
+  delay(600);
+  
+  tone(3,MIB,300);
+  delay(350);
+  noTone(3);
+  tone(3,sib,300);
+  delay(350);
+  noTone(3);
+  tone(3,solb,300);
+  delay(600);
+  noTone(3);
+  
+  tone(3,MIB,300);
+  delay(750);
+  noTone(3);
+  tone(3,sib,300);
+  delay(350);
+  noTone(3);
+  tone(3,solb,300);
+  delay(800);
+  noTone(3);
+  
+  
+  tone(3,SOL,300);
+  delay(600);
+  noTone(3);
+  tone(3,sol,300);
+  delay(350);
+  noTone(3);
+  tone(3,sol,300);
+  delay(350);
+  noTone(3);
+  tone(3,SOL,300);
+  delay(600);
+  noTone(3);
+        
+  tone(3,FAS,300);
+  delay(350);
+  noTone(3);
+  tone(3,FA,300);
+  delay(350);
+  noTone(3);
+  tone(3,MI,300);
+  delay(600);
+  noTone(3);
+  tone(3,RES,300);
+  delay(600);
+  noTone(3);
+        
+  tone(3,MI,300);
+  delay(600);
+  noTone(3);
+  tone(3,solS,300);
+  delay(350);
+  noTone(3);
+  tone(3,DOS,300);
+  delay(350);
+  noTone(3);
+  tone(3,DO,300);
+  delay(600);
+  noTone(3);
+        
+  tone(3,si,300);
+  delay(350);
+  noTone(3);      
+  tone(3,sib,300);
+  delay(350);
+  noTone(3);      
+  tone(3,la,300);     
+  delay(800);
+  noTone(3);      
+  tone(3,sib,300);
+  delay(600);
+  noTone(3);
+        
+   tone(3,mib,300);
+  delay(350);
+  noTone(3);
+  tone(3,solb,300);
+  delay(350);
+  noTone(3);
+  tone(3,mib,300);
+  delay(600);
+  noTone(3);
+  tone(3,solb,300);
+  delay(350);
+  noTone(3);
+        
+   tone(3,sib,300);
+  delay(350);
+  noTone(3);
+  tone(3,sol,300);
+  delay(600);
+  noTone(3);
+   tone(3,sib,300);
+  delay(600);
+  noTone(3);
+  tone(3,RE,300);
+  delay(600);
+  noTone(3);
+        
+   tone(3,SOL,300);
+  delay(350);
+  noTone(3);
+  tone(3,sol,300);
+  delay(350);
+  noTone(3);
+   tone(3,sol,300);
+  delay(600);
+  noTone(3);
+  tone(3,SOL,300);
+  delay(600);
+  noTone(3);
+  
+  tone(3,FAS,300);
+  delay(600);
+  tone(3,FA,300);
+  delay(350);
+  tone(3,MI,300);
+  delay(350);
+  
+  tone(3,RES,300);
+  delay(600);
+  noTone(3);
+        
+  tone(3,MI,300);
+  delay(700);
+  noTone(3);
+  tone(3,solS,300);
+  delay(350);
+  noTone(3);
+  tone(3,DOS,300);
+  delay(800);
+  noTone(3);
+  tone(3,DO,300);
+  delay(700);
+  noTone(3);
+  
+  tone(3,si,300);
+  delay(600);
+  noTone(3);
+  tone(3,sib,300);
+  delay(600);
+  noTone(3);
+  tone(3,la,300);
+  delay(600);
+  noTone(3);
+  tone(3,sib,300);
+  delay(350);
+  noTone(3);
+  
+  
+  tone(3,mib,300);
+  delay(350);
+  noTone(3);
+  tone(3,solb,300);
+  delay(600);
+  noTone(3);
+  
+  tone(3,mib,300);
+  delay(350);
+  noTone(3);
+  tone(3,sib,300);
+  delay(350);
+  noTone(3);
+  tone(3,sol,300);
+  delay(800);
+  noTone(3);
+  
+  tone(3,mib,300);
+  delay(600);
+  noTone(3);
+  tone(3,sib,300);
+  delay(600);
+  noTone(3);
+  tone(3,sol,300);
+  delay(600);
+  noTone(3);
+    
+}
+
+void loop()
+{  
+}
